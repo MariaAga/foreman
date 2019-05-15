29f4469ea85e52d7e6682af76e6765cdbd614260
magaphon <magaphon@localhost.localdomain>
2019-05-15 14:40:41 +0300

    Refs #26729 - Switch ajaxRequestActions in statistics

--- webpack/assets/javascripts/react_app/redux/actions/statistics/statistics.fixtures.js
+++ webpack/assets/javascripts/react_app/redux/actions/statistics/statistics.fixtures.js
@@ -1,69 +1,60 @@
-export const failedRequestData = [
-  {
+const payloads = {
+  operatingsystem: {
     id: 'operatingsystem',
+    search: '/hosts?search=os_title=~VAL~',
     title: 'OS Distribution',
     url: 'statistics/operatingsystem',
-    search: '/hosts?search=os_title=~VAL~',
   },
-  {
+  architecture: {
     id: 'architecture',
     title: 'Architecture Distribution',
     url: 'statistics/architecture',
     search: '/hosts?search=facts.architecture=~VAL~',
   },
+};
+
+export const failedRequestData = [
+  payloads.operatingsystem,
+  payloads.architecture,
 ];
 
 export const successRequestData = [
-  {
-    id: 'operatingsystem',
-    title: 'OS Distribution',
-    url: 'statistics/operatingsystem',
-    search: '/hosts?search=os_title=~VAL~',
-  },
-  {
-    id: 'architecture',
-    title: 'Architecture Distribution',
-    url: 'statistics/architecture',
-    search: '/hosts?search=facts.architecture=~VAL~',
-  },
+  payloads.operatingsystem,
+  payloads.architecture,
 ];
 
 export const onSuccessActions = [
   {
-    payload: {
-      id: 'operatingsystem',
-      search: '/hosts?search=os_title=~VAL~',
-      title: 'OS Distribution',
-      url: 'statistics/operatingsystem',
-    },
+    payload: payloads.operatingsystem,
     type: 'STATISTICS_DATA_REQUEST',
   },
   {
-    payload: {
-      id: 'architecture',
-      search: '/hosts?search=facts.architecture=~VAL~',
-      title: 'Architecture Distribution',
-      url: 'statistics/architecture',
-    },
+    payload: payloads.operatingsystem,
+    subtype: 'STATISTICS_DATA',
+    type: 'API_GET',
+    url: 'statistics/operatingsystem',
+  },
+  {
+    payload: payloads.architecture,
     type: 'STATISTICS_DATA_REQUEST',
+  },
+  {
+    payload: payloads.architecture,
+    subtype: 'STATISTICS_DATA',
+    type: 'API_GET',
+    url: 'statistics/architecture',
   },
   {
     payload: {
       data: [['centOS 7.1', 6]],
-      id: 'operatingsystem',
-      search: '/hosts?search=os_title=~VAL~',
-      title: 'OS Distribution',
-      url: 'statistics/operatingsystem',
+      ...payloads.operatingsystem,
     },
     type: 'STATISTICS_DATA_SUCCESS',
   },
   {
     payload: {
       data: [['x86_64', 6]],
-      id: 'architecture',
-      search: '/hosts?search=facts.architecture=~VAL~',
-      title: 'Architecture Distribution',
-      url: 'statistics/architecture',
+      ...payloads.architecture,
     },
     type: 'STATISTICS_DATA_SUCCESS',
   },
@@ -71,44 +62,36 @@ export const onSuccessActions = [
 
 export const onFailureActions = [
   {
-    payload: {
-      id: 'operatingsystem',
-      search: '/hosts?search=os_title=~VAL~',
-      title: 'OS Distribution',
-      url: 'statistics/operatingsystem',
-    },
+    payload: payloads.operatingsystem,
     type: 'STATISTICS_DATA_REQUEST',
   },
   {
-    payload: {
-      id: 'architecture',
-      search: '/hosts?search=facts.architecture=~VAL~',
-      title: 'Architecture Distribution',
-      url: 'statistics/architecture',
-    },
+    payload: payloads.operatingsystem,
+    subtype: 'STATISTICS_DATA',
+    type: 'API_GET',
+    url: 'statistics/operatingsystem',
+  },
+  {
+    payload: payloads.architecture,
     type: 'STATISTICS_DATA_REQUEST',
+  },
+  {
+    payload: payloads.architecture,
+    subtype: 'STATISTICS_DATA',
+    type: 'API_GET',
+    url: 'statistics/architecture',
   },
   {
     payload: {
       error: new Error('Request failed with status code 422'),
-      item: {
-        id: 'operatingsystem',
-        search: '/hosts?search=os_title=~VAL~',
-        title: 'OS Distribution',
-        url: 'statistics/operatingsystem',
-      },
+      payload: payloads.operatingsystem,
     },
     type: 'STATISTICS_DATA_FAILURE',
   },
   {
     payload: {
       error: new Error('Request failed with status code 422'),
-      item: {
-        id: 'architecture',
-        search: '/hosts?search=facts.architecture=~VAL~',
-        title: 'Architecture Distribution',
-        url: 'statistics/architecture',
-      },
+      payload: payloads.architecture,
     },
     type: 'STATISTICS_DATA_FAILURE',
   },
