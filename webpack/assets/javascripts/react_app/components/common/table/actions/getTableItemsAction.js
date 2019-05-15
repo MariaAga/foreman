c0ef5fef8ee3c53a87765fdaa97d275445e4af37
magaphon <magaphon@localhost.localdomain>
2019-05-15 14:41:34 +0300

    Refs #26729 - Switch ajaxRequestActions in table items

--- webpack/assets/javascripts/react_app/components/common/table/actions/getTableItemsAction.js
+++ webpack/assets/javascripts/react_app/components/common/table/actions/getTableItemsAction.js
@@ -1,6 +1,4 @@
 import URI from 'urijs';
-import { ajaxRequestAction } from '../../../../redux/actions/common';
-import createTableActionTypes from '../actionsHelpers/actionTypeCreator';
 
 /**
  * An async Redux action that fetches and stores table data in Redux.
@@ -8,20 +6,16 @@ import createTableActionTypes from '../actionsHelpers/actionTypeCreator';
  * @param  {Object} query      the API request query
  * @return {Function}          Redux Thunk function
  */
-const getTableItemsAction = (controller, query) => dispatch => {
+const getTableItemsAction = (controller, query) => {
   const url = new URI(`/api/${controller}`);
   url.addSearch({ ...query, include_permissions: true });
 
-  const ACTION_TYPES = createTableActionTypes(controller);
-
-  return ajaxRequestAction({
-    dispatch,
-    requestAction: ACTION_TYPES.REQUEST,
-    successAction: ACTION_TYPES.SUCCESS,
-    failedAction: ACTION_TYPES.FAILURE,
+  return {
+    type: 'API_GET',
+    subtype: controller,
     url: url.toString(),
-    item: { controller, url: url.toString() },
-  });
+    payload: { controller, url: url.toString() },
+  };
 };
 
 export default getTableItemsAction;
