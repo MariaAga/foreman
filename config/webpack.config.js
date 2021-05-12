1bfa942067e7f220bbfb76afff4de2b1459bfbd3
MariaAga <mariaagaphonchev@gmail.com>
2021-05-12 10:11:36 +0200

    test

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,10 +3,11 @@
 
 var path = require('path');
 var webpack = require('webpack');
-var ForemanVendorPlugin = require('@theforeman/vendor').WebpackForemanVendorPlugin;
+var ForemanVendorPlugin = require('@theforeman/vendor')
+  .WebpackForemanVendorPlugin;
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-var StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var StatsWriterPlugin = require('webpack-stats-plugin').StatsWriterPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var CompressionPlugin = require('compression-webpack-plugin');
 var pluginUtils = require('../script/plugin_webpack_directories');
 var vendorEntry = require('./webpack.vendor');
@@ -21,23 +22,25 @@ var args = argvParse({
   },
   host: {
     type: 'string',
-  }
-})
+  },
+});
 
 const supportedLocales = () => {
   const localeDir = path.join(__dirname, '..', 'locale');
 
   // Find all files in ./locale/*
-  const localesFiles = fs.readdirSync(localeDir)
+  const localesFiles = fs.readdirSync(localeDir);
 
   // Return only folders
-  return localesFiles.filter(f => fs.statSync(path.join(localeDir, f)).isDirectory());
-}
+  return localesFiles.filter(f =>
+    fs.statSync(path.join(localeDir, f)).isDirectory()
+  );
+};
 
 const supportedLanguages = () => {
   // Extract extract languages from the language tags (strip off dialects)
-  return [ ...new Set(supportedLocales().map(d => d.split('_')[0]))];
-}
+  return [...new Set(supportedLocales().map(d => d.split('_')[0]))];
+};
 
 const devServerConfig = () => {
   const result = require('dotenv').config();
@@ -48,8 +51,9 @@ const devServerConfig = () => {
   return {
     port: args.port || '3808',
     host: args.host || process.env.BIND || 'localhost',
-  }
-}
+    http2: false,
+  };
+};
 
 module.exports = env => {
   const devServer = devServerConfig();
@@ -58,29 +62,44 @@ module.exports = env => {
   var production =
     process.env.RAILS_ENV === 'production' ||
     process.env.NODE_ENV === 'production';
-
-  var bundleEntry = path.join(__dirname, '..', 'webpack/assets/javascripts/bundle.js');
+  const mode = production ? 'production' : 'development';
+  var bundleEntry = path.join(
+    __dirname,
+    '..',
+    'webpack/assets/javascripts/bundle.js'
+  );
 
   var plugins = pluginUtils.getPluginDirs('pipe');
 
-  var resolveModules = [  path.join(__dirname, '..', 'webpack'),
-                          path.join(__dirname, '..', 'node_modules'),
-                          'node_modules/',
-                       ].concat(pluginUtils.pluginNodeModules(plugins));
+  var resolveModules = [
+    path.join(__dirname, '..', 'webpack'),
+    path.join(__dirname, '..', 'node_modules'),
+    'node_modules/',
+  ].concat(pluginUtils.pluginNodeModules(plugins));
 
   if (env && env.pluginName !== undefined) {
     var pluginEntries = {};
     pluginEntries[env.pluginName] = plugins['entries'][env.pluginName];
     for (var entry of Object.keys(plugins['entries'])) {
-      if (entry.startsWith(env.pluginName + ":")) {
-          pluginEntries[entry] = plugins['entries'][entry]
+      if (entry.startsWith(env.pluginName + ':')) {
+        pluginEntries[entry] = plugins['entries'][entry];
       }
-    };
+    }
 
-    var outputPath = path.join(plugins['plugins'][env.pluginName]['root'], 'public', 'webpack');
-    var jsFilename = production ? env.pluginName + '/[name]-[chunkhash].js' : env.pluginName + '/[name].js';
-    var cssFilename = production ? env.pluginName + '/[name]-[chunkhash].css' : env.pluginName + '/[name].css';
-    var chunkFilename = production ? env.pluginName + '/[name]-[chunkhash].js' : env.pluginName + '/[name].js';
+    var outputPath = path.join(
+      plugins['plugins'][env.pluginName]['root'],
+      'public',
+      'webpack'
+    );
+    var jsFilename = production
+      ? env.pluginName + '/[name]-[chunkhash].js'
+      : env.pluginName + '/[name].js';
+    var cssFilename = production
+      ? env.pluginName + '/[name]-[chunkhash].css'
+      : env.pluginName + '/[name].css';
+    var chunkFilename = production
+      ? env.pluginName + '/[name]-[chunkhash].js'
+      : env.pluginName + '/[name].js';
     var manifestFilename = env.pluginName + '/manifest.json';
   } else {
     var pluginEntries = plugins['entries'];
@@ -90,7 +109,7 @@ module.exports = env => {
     var chunkFilename = production ? '[name]-[chunkhash].js' : '[name].js';
     var manifestFilename = 'manifest.json';
   }
-
+  console.log({ manifestFilename, production });
   var entry = Object.assign(
     {
       bundle: bundleEntry,
@@ -99,10 +118,13 @@ module.exports = env => {
     pluginEntries
   );
 
-  const supportedLanguagesRE = new RegExp(`/(${supportedLanguages().join('|')})$`);
+  const supportedLanguagesRE = new RegExp(
+    `/(${supportedLanguages().join('|')})$`
+  );
 
   var config = {
     entry: entry,
+    mode,
     output: {
       // Build assets directly in to public/webpack/, let webpack know
       // that all webpacked assets start with webpack/
@@ -111,18 +133,20 @@ module.exports = env => {
       path: outputPath,
       publicPath: '/webpack/',
       filename: jsFilename,
-      chunkFilename
+      chunkFilename,
     },
 
     resolve: {
       modules: resolveModules,
-      alias: Object.assign({
-        foremanReact:
-          path.join(__dirname,
-             '../webpack/assets/javascripts/react_app'),
+      alias: Object.assign(
+        {
+          foremanReact: path.join(
+            __dirname,
+            '../webpack/assets/javascripts/react_app'
+          ),
         },
         pluginUtils.aliasPlugins(pluginEntries)
-      )
+      ),
     },
 
     module: {
@@ -134,60 +158,70 @@ module.exports = env => {
           exclude: /node_modules(?!\/(@novnc|unidiff))/,
           loader: 'babel-loader',
           options: {
-            presets: [require.resolve('@theforeman/builder/babel')]
+            presets: [require.resolve('@theforeman/builder/babel')],
           },
         },
+        // {
+        //   test: /\.css$/i,
+        //   use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        // },
         {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: 'css-loader'
-          })
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            production ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
         },
         {
           test: /\.(png|gif|svg)$/,
-          use: 'url-loader?limit=32767'
+          type: 'asset/inline',
         },
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader', // The backup style loader
-            use: production ? 'css-loader!sass-loader' : 'css-loader?sourceMap!sass-loader?sourceMap'
-          })
-        },
+        // {
+        //   test: /\.scss$/,
+        //   use: [
+        //     MiniCssExtractPlugin.loader,
+        //     'css-loader!sass-loader'
+        //     // production
+        //     //   ? 'css-loader!sass-loader'
+        //     //   : 'css-loader?sourceMap!sass-loader?sourceMap',
+        //   ],
+        // },
         {
           test: /\.(graphql|gql)$/,
           exclude: /node_modules/,
           loader: 'graphql-tag/loader',
-        }
-      ]
+        },
+      ],
     },
 
     plugins: [
-      new ForemanVendorPlugin({ mode: production ? 'production' : 'development' }),
+      new ForemanVendorPlugin({ mode }),
       // must match config.webpack.manifest_filename
       new StatsWriterPlugin({
         filename: manifestFilename,
         fields: null,
-        transform: function (data, opts) {
+        transform: function(data) {
           return JSON.stringify(
             {
               assetsByChunkName: data.assetsByChunkName,
               errors: data.errors,
-              warnings: data.warnings
-            }, null, 2 );
-        }
+              warnings: data.warnings,
+            },
+            null,
+            2
+          );
+        },
       }),
-      new ExtractTextPlugin({
+      new MiniCssExtractPlugin({
         filename: cssFilename,
-        allChunks: true
       }),
       new webpack.DefinePlugin({
         'process.env': {
-          NODE_ENV: JSON.stringify(production ? 'production' : 'development'),
+          NODE_ENV: JSON.stringify(mode),
           NOTIFICATIONS_POLLING: process.env.NOTIFICATIONS_POLLING,
-          REDUX_LOGGER: process.env.REDUX_LOGGER
-        }
+          REDUX_LOGGER: process.env.REDUX_LOGGER,
+        },
       }),
       // limit locales from intl only to supported ones
       new webpack.ContextReplacementPlugin(
@@ -199,25 +233,32 @@ module.exports = env => {
         /react-intl\/locale-data/,
         supportedLanguagesRE
       ),
-    ]
+    ],
+  };
+  config.optimization = {
+    splitChunks: {
+      cacheGroups: {
+        vendor1: {
+          name: 'vendor1',
+          // chunks: 'all',
+          enforce: true,
+        },
+        vendors: false,
+        default: false,
+      },
+    },
   };
 
-  config.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    minChunks: Infinity,
-  }))
 
   if (production) {
     config.plugins.push(
-      new webpack.NoEmitOnErrorsPlugin(),
       new UglifyJsPlugin({
         uglifyOptions: {
           compress: { warnings: false },
         },
-        sourceMap: true
+        sourceMap: true,
       }),
       new SimpleNamedModulesPlugin(),
-      new webpack.optimize.ModuleConcatenationPlugin(),
       new webpack.optimize.OccurrenceOrderPlugin(),
       new CompressionPlugin()
     );
@@ -226,16 +267,27 @@ module.exports = env => {
     config.plugins.push(
       new webpack.HotModuleReplacementPlugin() // Enable HMR
     );
-
     config.devServer = {
       host: devServer.host,
       port: devServer.port,
       headers: { 'Access-Control-Allow-Origin': '*' },
-      hot: true
+      hot: true,
     };
+    // https://github.com/webpack/webpack-dev-server/issues/2996
+    // if (args.https) {
+    //   config.devServer.https = {
+    //     key: args.key,
+    //     cert: args.cert,
+    //     ca: args.cacert,
+    //   };
+    // }
     // Source maps
     config.devtool = 'inline-source-map';
   }
+  console.log('\n\n\n\n\n');
+  console.log({ dev: config.devServer });
+  console.log({ args });
+  console.log('\n\n\n\n\n');
 
   return config;
-}
+};
