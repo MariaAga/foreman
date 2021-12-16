6acff7ab30799a0feeaf1bdc87f13f005dc7a762
MariaAga <mariaaga@redhat.com>
2021-12-16 13:54:24 +0100

    temp

--- webpack/assets/javascripts/react_app/components/common/ActionButtons/ActionButtons.js
+++ webpack/assets/javascripts/react_app/components/common/ActionButtons/ActionButtons.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { SplitButton, MenuItem, Button } from 'patternfly-react';
+// import { SplitButton, MenuItem, Button } from 'patternfly-react';
 
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownToggleAction,
+} from '@patternfly/react-core';
+import { CaretDownIcon } from '@patternfly/react-icons';
 /**
  * Generate a button or a dropdown of buttons
  * @param  {String} title The title of the button for the title and text inside the button
@@ -9,29 +17,42 @@ import { SplitButton, MenuItem, Button } from 'patternfly-react';
  * @return {Function} button component or splitbutton component
  */
 export const ActionButtons = ({ buttons }) => {
+  const [isOpen, setIsOpen] = useState(false);
   if (!buttons.length) return null;
   if (buttons.length === 1)
-    return (
-      <Button bsSize="small" {...buttons[0].action}>
-        {buttons[0].title}
-      </Button>
-    );
+    return <Button {...buttons[0].action}>{buttons[0].title}</Button>;
   const firstButton = buttons.shift();
   return (
-    <SplitButton
-      title={firstButton.title}
-      {...firstButton.action}
-      bsSize="small"
-    >
-      {buttons.map(button => (
-        <MenuItem key={button.title} title={button.title} {...button.action}>
+    <Dropdown
+      toggle={
+        <DropdownToggle
+          splitButtonItems={[
+            <DropdownToggleAction
+              key={firstButton.title}
+              {...firstButton.action}
+            >
+              title={firstButton.title}
+            </DropdownToggleAction>,
+          ]}
+          onToggle={newIsOpen => setIsOpen(newIsOpen)}
+          toggleIndicator={CaretDownIcon}
+          title={firstButton.title}
+        />
+      }
+      isOpen={isOpen}
+      dropdownItems={buttons.map(button => (
+        <DropdownItem
+          key={button.title}
+          component="button"
+          title={button.title}
+          {...button.action}
+        >
           {button.title}
-        </MenuItem>
+        </DropdownItem>
       ))}
-    </SplitButton>
+    />
   );
 };
-
 ActionButtons.propTypes = {
   buttons: PropTypes.arrayOf(
     PropTypes.shape({
